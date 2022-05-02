6cc5dc3d12c39da935e1fc70264c2f7115dd8ae3
ledshra45@gmail.com <ledshra45@gmail.com>
2022-05-01 23:09:12 -0500

    login con connecion a la base de datos

--- index.php
+++ index.php
@@ -1,21 +1,63 @@
-<!DOCTYPEhtml>
+<?php session_start(); ?>
 <html>
-    <head>
-      <LINK REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
-    </head>
-    <body>
-    
+<head>
+    <title>Login</title>
+</head>
+
+<body>
+<?php
+include("connection.php");
+
+if(isset($_POST['submit'])) {
+    $user = mysqli_real_escape_string($mysqli, $_POST['username']);
+    $pass = mysqli_real_escape_string($mysqli, $_POST['password']);
+
+    if($user == "" || $pass == "") {
+        echo "<center>Usuario y Comtraseña Vacio.";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<a href='login.php'>Volver</a>";
+    } else {
+        $result = mysqli_query($mysqli, "SELECT * FROM login WHERE username='$user' AND password=md5('$pass')")
+        or die("ERROR.");
+		
+        $row = mysqli_fetch_assoc($result);
+		
+        if(is_array($row) && !empty($row)) {
+            $validuser = $row['username'];
+            $_SESSION['valid'] = $validuser;
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['id'] = $row['id'];
+        } else {
+            echo "<center>Usuario o Contraseña Incorrecto.";
+            echo "<br/>";
+            echo "<br/>";
+            echo "<a href='login.php'>Volver</a>";
+        }
+
+        if(isset($_SESSION['valid'])) {
+            header('Location: index.php');			
+        }
+    }
+} else {
+?>
+    <form name="form1" method="post" action="">
+    <h1>Login</h1>
+        <table>
+             Usuario
+             <br>
+                <input type="text" name="username">
+             <br>
+             Contraseña
+             <br>
+                <input type="password" name="password">
+            <br>
+                <td><input type="submit" name="submit" value="Submit"></td>
             
-  <div class='register'>
-  <center>
-  <h1>Inice Seción</h1>
-  usuario<br>
-  <input type="text" name="" placeholder="usuario">
-  <br><br />
-  <input type="password" name="" placeholder="contraseña">
-  <br/><br />
-  <input type="submit" name="" value="login">
-  <input type="submit" name="" value="registro">
-  </form></center>
-    </body>
+        </table>
+    </form>
+<?php
+}
+?>
+</body>
 </html>
